342a7aae33b833c4b25082f24d2f1201fe51567a
Lucas Carvalho <antoniolucas@live.com>
2025-06-28 13:38:22 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mqtt = require('mqtt');
+const app = express();
+app.use(express.json());
+
+const client = mqtt.connect('mqtt://broker.hivemq.com');
+
+client.on('connect', () => {
+  console.log('MQTT conectado');
+});
+
+app.post('/comando', (req, res) => {
+  const comando = req.body.comando;
+  if (!comando) return res.status(400).send("Comando inválido");
+  client.publish('carrinho/comando', comando);
+  res.send("Comando enviado: " + comando);
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Servidor online");
+});
